46adfc9ad54f7bfeb156e69a4931a667014b520b
Captain Fulminyx <captainfulminyx@gmail.com>
2025-12-20 09:47:14 +0000

    Update v0.1 feat:header

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router'
+import HomeView from '#/HomePage.vue'
+import AboutView from '#/CoffeePage.vue'
 
-const router = createRouter({
+const router = createRouter( {
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{
+    path: '/', component: HomeView
+  },
+    {
+      path: '/about', component: AboutView
+    },
+  ],
 })
 
 export default router
--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,27 @@ import {
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import Components from 'unplugin-vue-components/vite';
+
+
 // https://vite.dev/config/
 export default defineConfig( {
   plugins: [
     vue(),
     vueDevTools(),
     tailwindcss(),
+    Icons(),
+    Components( {
+      resolvers: [IconsResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "$": fileURLToPath(new URL("./src/components", import.meta.url)),
+      "#": fileURLToPath(new URL("./src/pages", import.meta.url))
     },
   },
 })
