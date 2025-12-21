c3857ebfce8deb0d8bcb4066fb06378653df0efa
Captain Fulminyx <captainfulminyx@gmail.com>
2025-12-21 09:00:55 +0000

    Update 0.2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import Header from "$/Header.vue";
+import Drawer from "$/Drawer.vue";
 </script>
 
 <template>
-    <div class="w-screen h-screen bg-prim fixed p-2">
+    <div class="w-screen h-screen bg-bg fixed p-2">
         <Header />
     </div>
 </template>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div class="w-full h-full flex bg-brute">
+        <div class="drawer">
+            <i-mdi-close class="text-ltxt size-7 absolute top-2 right-2" />
+        </div>
+    </div>
+</template>
