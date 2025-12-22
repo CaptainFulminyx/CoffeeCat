ed49fe0428543ac54ec08558a9d7c4af1c56da15
Captain Fulminyx <captainfulminyx@gmail.com>
2025-12-22 12:02:59 +0000

    0.4 update:minor

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,9 +1,14 @@
-<script setup></script>
-
+<script setup>
+let visible = true;
+function close() {
+    visible = false;
+}
+</script>
 <template>
-  
-        <div class="drawer">
-            <i-mdi-close class="text-ltxt size-7 absolute top-2 right-2" />
-        </div>
-  
+    <div class="drawer" v-if="visible">
+        <i-mdi-close
+            class="text-ltxt size-7 absolute top-2 right-2"
+            @click="close()"
+        />
+    </div>
 </template>
