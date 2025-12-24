08a53b0fab72c73274a4b7061c4a9187b7d32a61
Captain Fulminyx <captainfulminyx@gmail.com>
2025-12-24 14:41:55 +0000

    Update: v0.4.5 styling

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,13 @@
 import { ref } from "vue";
 import Header from "$/Header.vue";
 import Drawer from "$/Drawer.vue";
+import Home from "#/HomePage.vue";
 </script>
 
 <template>
     <div class="w-screen h-screen bg-bg fixed p-2">
         <Header />
-        <Drawer>
-            <h2 class="sm:text-lg lg:text-2xl font-semibold mb-2">Menu</h2>
-            <ul class="space-y-2">
-                <li>Home</li>
-                <li>Orders</li>
-                <li>Profile</li>
-            </ul>
-        </Drawer>
+        <Drawer />
+        <Home />
     </div>
 </template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,9 @@
-<script setup>
-</script>
+<script setup></script>
 <template>
-  
+    <div class="container">
+        <div class="hero">
+            <p>Hello, Cat 😺!</p>
+        </div>
+        <div class="section"></div>
+    </div>
 </template>
