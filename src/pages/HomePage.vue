36bfcced31dbe68aafc7f0b54e34b236de69f7d1
Captain Fulminyx <captainfulminyx@gmail.com>
2025-12-27 14:17:50 +0000

    ...

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,19 +1,38 @@
 <script setup></script>
 
 <template>
-    <div class="hero">
+    <section class="hero">
         <p class="head">Find your coffee, Build your habit</p>
         <p class="main">
             CoffeeCat helps you find the perfect brew and track what you drink.
             Start simple, stay consistent.
         </p>
         <span class="row">
-          <button>Explore</button>
-          <button>Learn</button>
+            <button>Explore</button>
+            <button>Learn</button>
         </span>
-    </div>
-    <div class="body"></div>
-    <div class="fb"></div>
-    <div class="faq"></div>
-    <div class="footer"></div>
+    </section>
+    <section class="home-body">
+        <h2 class="section-title"></h2>
+        <h1 class="title"></h1>
+        <p class="text"></p>
+        <div class="section-card">
+            <h3 class="card-h2"></h3>
+            <h2 class="card-h2"></h2>
+            <p></p>
+        </div>
+        <div class="section-card">
+            <h3 class="card-h2"></h3>
+            <h2 class="card-h2"></h2>
+            <p></p>
+        </div>
+        <div class="section-card">
+            <h3 class="card-h2"></h3>
+            <h2 class="card-h2"></h2>
+            <p></p>
+        </div>
+    </section>
+    <section class="fb"></section>
+    <section class="faq"></section>
+    <section class="footer"></section>
 </template>
